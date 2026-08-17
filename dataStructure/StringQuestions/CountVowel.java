8e51a70fe432db03704fd1c26a9bc58856ba9b39
Ishant <ishantiit@gmail.com>
2026-08-18 03:28:07 +0530

    Interview question practice: String

---
+++ dataStructure/StringQuestions/CountVowel.java
@@ -0,0 +1,24 @@
+package dataStructure.StringQuestions;
+
+public class CountVowel {
+    public static int countVowel(String str){
+        int countV =0;
+        int countC = 0;
+        for(char c : str.toCharArray()){
+            if(isVowel(c)){
+                countV++;
+            } else{
+                countC++;
+            }
+        }
+        System.out.println("Consonant count: " + countC);
+        return countV;
+    }
+    static boolean isVowel(char c){
+        return 'a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c;
+    }
+    public static void main(String[] args){
+        String str = "pankhudi";
+        System.out.println("Vowel count "+countVowel(str));
+    }
+}
