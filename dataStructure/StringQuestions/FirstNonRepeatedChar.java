0f2e96558393d471075e9ebf0a45ba349b486b20
Ishant <ishantiit@gmail.com>
2026-08-18 03:11:50 +0530

    Interview question practice: String

---
+++ dataStructure/StringQuestions/FirstNonRepeatedChar.java
@@ -0,0 +1,23 @@
+package dataStructure.StringQuestions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepeatedChar {
+    public static char firstNonRepeatedChar(String str){
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for(char c : str.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+                return entry.getKey();
+            }
+        } return ' ';
+    }
+    public static void main(String[] args){
+        String str = "SWISS";
+        System.out.println(firstNonRepeatedChar(str));
+    }
+}
