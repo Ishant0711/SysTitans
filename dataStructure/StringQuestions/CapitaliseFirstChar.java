21ce620e4112b542a2ce733bf67fec934eadff11
Ishant <ishantiit@gmail.com>
2026-08-21 01:14:42 +0530

    Interview question practice: String

--- dataStructure/StringQuestions/CapitaliseFirstChar.java
+++ dataStructure/StringQuestions/CapitaliseFirstChar.java
@@ -3,12 +3,21 @@ package dataStructure.StringQuestions;
 public class CapitaliseFirstChar {
     public static String capitaliseFirstChar(String st){
         StringBuilder ss = new StringBuilder();
-        String[] str = st.trim().split(" ");
-        for(String sp :str){
-            char[] ch  = sp.toCharArray();
-            ch[0] = Character.toUpperCase(ch[0]);
-           // String result = new String(ch);
-            ss.append(ch).append(" ");
+        String[] str = st.trim().split("\\s+");
+
+//        for(String sp :str){
+//            char[] ch  = sp.toCharArray();
+//            ch[0] = Character.toUpperCase(ch[0]);
+//            //String result = new String(ch);
+//            ss.append(ch).append(" ");
+//        }
+
+        for(String sp : str){
+            if(!sp.isEmpty()){
+                ss.append(Character.toUpperCase(sp.charAt(0)))
+                        .append(sp.substring(1))
+                        .append(" ");
+            }
         }
         return ss.toString();
     }
